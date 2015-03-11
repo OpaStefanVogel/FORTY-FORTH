36756a06fe1cf671d64d837ef43276538ea1e9fb
Stefan, beim Spartan3 druecken <backebackekuchen16@gmail.com>
2015-03-11 09:21:17 +0100

    Spartan3_Step_9 extra

--- TopSpartan3fuerGitHub.vhd
+++ TopSpartan3fuerGitHub.vhd
@@ -50,12 +50,7 @@ architecture Step_9_und_10 of TopSpartan3fuerGitHub is
     -- EMIT --
     EMIT_ABGESCHICKT: out STD_LOGIC;
     EMIT_BYTE: out STD_LOGIC_VECTOR (7 downto 0);
-    EMIT_ANGEKOMMEN: in STD_LOGIC;
-
-     -- KEY --
-    KEY_ABGESCHICKT: in STD_LOGIC;
-    KEY_BYTE: in STD_LOGIC_VECTOR (7 downto 0);
-    KEY_ANGEKOMMEN: out STD_LOGIC
+    EMIT_ANGEKOMMEN: in STD_LOGIC
 
     );
 end component;
@@ -90,13 +85,7 @@ DUT: top
     -- EMIT --
     EMIT_ABGESCHICKT   => EMIT_ABGESCHICKT,
     EMIT_BYTE       => EMIT_BYTE,
-    EMIT_ANGEKOMMEN  => EMIT_ANGEKOMMEN,
-
-     -- KEY --
-    KEY_ABGESCHICKT  => KEY_ABGESCHICKT,
-    KEY_BYTE      => KEY_BYTE,
-    KEY_ANGEKOMMEN => open
-
+    EMIT_ANGEKOMMEN  => EMIT_ANGEKOMMEN
 
     );
 
