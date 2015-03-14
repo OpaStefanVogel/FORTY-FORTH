f377d2ae552a7c9a05b4f9ca0af8222cde824879
Stefan, beim Spartan3 druecken <backebackekuchen16@gmail.com>
2015-03-14 22:43:19 +0100

    Revert "TXD<=RXD"
    This reverts commit 9549c23968d072cf305330981dccd8251af38e92.
    
    wieder zusammenführen

--- TopSpartan3fuerGitHub.vhd
+++ TopSpartan3fuerGitHub.vhd
@@ -95,7 +95,7 @@ process(CLK) begin if CLK'event and CLK='1' then
   CLK_I<=TAKTZAEHLER(21);
   end if; end process;
 
-TXD <= RXD;
+TXD <= '1';
 
     AL <= "000000000000000000";
     DL <= x"00000000";
