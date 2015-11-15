b4c2fd4c5b001b3f96fa67730bf26006f7d8fdd0
Stefan_vom_Raspberry_aus <backebackekuchen16@gmail.com>
2015-11-15 21:11:05 +0100

    generate mit LABEL und danach Semikolon, in generic Semikolon erst nach Klammer zu

--- P01_top.vhd
+++ P01_top.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-  generic ( NJ : integer := 2 ; NK : integer := 2 ; )
+  generic ( NJ : integer := 2 ; NK : integer := 2 );
   Port ( 
     CLK: in STD_LOGIC;
     LEDS: out STD_LOGIC_VECTOR (7 downto 0);
@@ -100,7 +100,7 @@ signal PC, PD, A, B, C, D, SP : CON16 ;
 begin
 
   -- component instantiation
-for L in 0 to NJ*NK-1 generate
+LABEL_1: for L in 0 to NJ*NK-1 generate
 DUTL: FortyForthProcessor
   port map (
     CLK_I => CLK,DAT_I => DAT_I(L),ADR_O => ADR(L),DAT_O => DAT_O(L),WE_O => WE(L),
@@ -112,29 +112,29 @@ DUTL: FortyForthProcessor
      UNTEN_ABGESCHICKT => U_AB(L), UNTEN_DAT => U_DAT(L), UNTEN_ADR => U_ADR(L), UNTEN_ANGEKOMMEN => U_AN(L),-- UNTEN --
     PC_SIM => PC(L),PD_SIM => PD(L),A_SIM => A(L),B_SIM => B(L),C_SIM => C(L),D_SIM => D(L),SP_SIM => SP(L) -- nur fuer Simulation
     );
-  if L>0 generate E_AN(L) <= E_AB(L) ; end generate ;
-  end generate
+LABEL_2:  if L>0 generate E_AN(L) <= E_AB(L) ; end generate ;
+  end generate ;
 
-for J in 0 to NJ-1 generate
-  for K in 0 to NK-1 generate
-    if K=0 generate
+LABEL_3: for J in 0 to NJ-1 generate
+LABEL_4:   for K in 0 to NK-1 generate
+LABEL_5:     if K=0 generate
       L_AB(J*NK)<=R_AB(J*NK+NK-1); L_DAT(J*NK)<=R_DAT(J*NK+NK-1);
       R_AN(J*NK+NK-1)<=L_AN(J*NK); R_ADR(J*NK+NK-1)<=L_ADR(J*NK);
-      end generate
-    if K>0 generate
+      end generate ;
+LABEL_6:     if K>0 generate
       L_AB(J*NK+K)<=R_AB(J*NK+K-1); L_DAT(J*NK+K)<=R_DAT(J*NK+K-1);
       R_AN(J*NK+K-1)<=L_AN(J*NK+K); R_ADR(J*NK+K-1)<=L_ADR(J*NK+K);
-      end generate
-    if J=0 generate
+      end generate ;
+LABEL_7:     if J=0 generate
       O_AB(K)<=U_AB((NJ-1)*NK+K); O_DAT(K)<=U_DAT((NJ-1)*NK+K);
       U_AN((NJ-1)*NK+K)<=O_AN(K); U_ADR((NJ-1)*NK+K)<=O_ADR(K);
-      end generate
-    if J>0 generate
+      end generate ;
+LABEL_8:     if J>0 generate
       O_AB(J*NK+K)<=U_AB((J-1)*NJ+K); O_DAT(J*NK+K)<=U_DAT((J-1)*NJ+K);
       U_AN((J-1)*NJ+K)<=O_AN(J*NK+K); U_ADR((J-1)*NJ+K)<=O_ADR(J*NK+K);
-      end generate
-    end generate
-  end generate
+      end generate ;
+    end generate ;
+  end generate ;
 
 -- L_AB(1)<=R_AB(0); L_DAT(1)<=R_DAT(0); R_ADR(0)<=L_ADR(1); R_AN(0)<=L_AN(1);
 -- L_AB(0)<=R_AB(1); L_DAT(0)<=R_DAT(1); R_ADR(1)<=L_ADR(0); R_AN(1)<=L_AN(0);
@@ -166,15 +166,15 @@ C_SIM <= C(0) ;
 D_SIM <= D(0) ;
 SP_SIM <= SP(0) ;
 
-process begin wait until (CLK'event and CLK='1');
-  for L in 0 to NJ*NK-1 generate
-    if L<8 generate
-      if WE(L)='1' then
-        if ADR(L)=x"2D04" then LEDS(L)<=DAT(L)(L); end if; 
-        end if;
-      end generate;
-    end generate;
-  end process;
+--process begin wait until (CLK'event and CLK='1');
+--LABEL_9:   for L in 0 to NJ*NK-1 generate
+--LABEL_10:    if L<8 generate
+--      if WE(L)='1' then
+--        if ADR(L)=x"2D04" then LEDS(L)<=DAT(L)(L); end if; 
+--        end if;
+--      end generate;
+--    end generate;
+--  end process;
 
 end Step_1;
 
