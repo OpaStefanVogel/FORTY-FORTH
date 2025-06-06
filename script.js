acc29f752d1524c036202143d1a61edb59d94a2f
Stefan_MGit <backebackekuchen16@gmail.com>
2025-06-06 19:34:42 +0200

    gpu-compute-sample-2.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ console.log('script');
 
 console.log('device ist da');
 
+/*
   // Get a GPU buffer in a mapped state and an arrayBuffer for writing.
   const gpuWriteBuffer = device.createBuffer({
     mappedAtCreation: true,
@@ -48,11 +49,11 @@ console.log('gpuReadBuffer ist erzeugt: '+gpuReadBuffer);
 
   const copyEncoder = device . createCommandEncoder();
   copyEncoder . copyBufferToBuffer(
-    gpuWriteBuffer /* source buffer */,
-    0 /* source offset */,
-    gpuReadBuffer /* destination buffer */,
-    0 /* destination offset */,
-    4 /* size */
+    gpuWriteBuffer,
+    0,
+    gpuReadBuffer,
+    0,
+    4
     );
     
 console.log('copyEncoder ist erzeugt und gefüllt: '+copyEncoder);
@@ -69,7 +70,215 @@ console.log('await gpuReadBuffer . mapAsync ');
 
 console.log('copyArrayBuffer ist erzeugt: '+copyArrayBuffer);
 
-console.log('Ergebnis: '+(new Uint8Array ( copyArrayBuffer) ) ) ;
+console.log('Ergebnis: '+(new Uint8Array ( copyArrayBuffer) +'--------------------------------------') ) ;
+
+*/
+
+console.log('jetzt das Beispiel 2');
+
+
+// First Matrix
+
+const firstMatrix = new Float32Array([
+  2 /* rows */, 4 /* columns */,
+  1, 2, 3, 4,
+  5, 6, 7, 8
+]);
+
+const gpuBufferFirstMatrix = device.createBuffer({
+  mappedAtCreation: true,
+  size: firstMatrix.byteLength,
+  usage: GPUBufferUsage.STORAGE,
+});
+
+console.log('gpuBufferFirstMatrix ist erzeugt: '+gpuBufferFirstMatrix);
+
+
+const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
+new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
+gpuBufferFirstMatrix.unmap();
+
+console.log('arrayBufferFirstMatfix ist erzeugt und gefüllt und .ummap(): '+arrayBufferFirstMatrix);
+
+
+// Second Matrix
+
+const secondMatrix = new Float32Array([
+  4 /* rows */, 2 /* columns */,
+  1, 2,
+  3, 4,
+  5, 6,
+  7, 8
+]);
+
+const gpuBufferSecondMatrix = device.createBuffer({
+  mappedAtCreation: true,
+  size: secondMatrix.byteLength,
+  usage: GPUBufferUsage.STORAGE,
+});
+const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
+new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
+gpuBufferSecondMatrix.unmap();
+
+console.log('arrayBufferSecondMatfix ist erzeugt und gefüllt und .ummap(): '+arrayBufferFirstMatrix);
+
+// Result Matrix
+
+const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[1]);
+const resultMatrixBuffer = device.createBuffer({
+  size: resultMatrixBufferSize,
+  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+});
+
+console.log('resultMatrixBuffer ist erzeugt:'+resultMatrixBuffer);
+
+
+const bindGroupLayout = device.createBindGroupLayout({
+  entries: [
+    {
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: {
+        type: "read-only-storage"
+      }
+    },
+    {
+      binding: 1,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: {
+        type: "read-only-storage"
+      }
+    },
+    {
+      binding: 2,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: {
+        type: "storage"
+      }
+    }
+  ]
+});
+
+const bindGroup = device.createBindGroup({
+  layout: bindGroupLayout,
+  entries: [
+    {
+      binding: 0,
+      resource: {
+        buffer: gpuBufferFirstMatrix
+      }
+    },
+    {
+      binding: 1,
+      resource: {
+        buffer: gpuBufferSecondMatrix
+      }
+    },
+    {
+      binding: 2,
+      resource: {
+        buffer: resultMatrixBuffer
+      }
+    }
+  ]
+});
+
+console.log('bindGroup ist erzeugt wofür auch immer:'+bindGroup);
+
+
+
+
+const shaderModule = device.createShaderModule({
+  code: `
+    struct Matrix {
+      size : vec2f,
+      numbers: array<f32>,
+    }
+
+    @group(0) @binding(0) var<storage, read> firstMatrix : Matrix;
+    @group(0) @binding(1) var<storage, read> secondMatrix : Matrix;
+    @group(0) @binding(2) var<storage, read_write> resultMatrix : Matrix;
+
+    @compute @workgroup_size(8, 8)
+    fn main(@builtin(global_invocation_id) global_id : vec3u) {
+      // Guard against out-of-bounds work group sizes
+      if (global_id.x >= u32(firstMatrix.size.x) || global_id.y >= u32(secondMatrix.size.y)) {
+        return;
+      }
+
+      resultMatrix.size = vec2(firstMatrix.size.x, secondMatrix.size.y);
+
+      let resultCell = vec2(global_id.x, global_id.y);
+      var result = 0.0;
+      for (var i = 0u; i < u32(firstMatrix.size.y); i = i + 1u) {
+        let a = i + resultCell.x * u32(firstMatrix.size.y);
+        let b = resultCell.y + i * u32(secondMatrix.size.y);
+        result = result + firstMatrix.numbers[a] * secondMatrix.numbers[b];
+      }
+      result=result*10.0;
+      let index = resultCell.y + resultCell.x * u32(secondMatrix.size.y);
+      resultMatrix.numbers[index] = result;
+    }
+  `
+});
+
+console.log('shaderModule ist erzeugt wofür auch immer:'+shaderModule);
+
+
+const computePipeline = device.createComputePipeline({
+  layout: device.createPipelineLayout({
+    bindGroupLayouts: [bindGroupLayout]
+  }),
+  compute: {
+    module: shaderModule,
+    entryPoint: "main"
+  }
+});
+
+console.log('computePipeline ist erzeugt wofür auch immer:'+computePipeline);
+
+
+
+const commandEncoder = device.createCommandEncoder();
+
+const passEncoder = commandEncoder.beginComputePass();
+passEncoder.setPipeline(computePipeline);
+passEncoder.setBindGroup(0, bindGroup);
+const workgroupCountX = Math.ceil(firstMatrix[0] / 8);
+const workgroupCountY = Math.ceil(secondMatrix[1] / 8);
+passEncoder.dispatchWorkgroups(workgroupCountX, workgroupCountY);
+passEncoder.end();
+
+console.log('passEncoder.end wie auch immer:'+passEncoder);
+console.log('mit Math.ceil(firstMatrix[0] / 8)='+Math.ceil(firstMatrix[0] / 8));
+
+
+// Get a GPU buffer for reading in an unmapped state.
+const gpuReadBuffer = device.createBuffer({
+  size: resultMatrixBufferSize,
+  usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+});
+
+// Encode commands for copying buffer to buffer.
+commandEncoder.copyBufferToBuffer(
+  resultMatrixBuffer /* source buffer */,
+  0 /* source offset */,
+  gpuReadBuffer /* destination buffer */,
+  0 /* destination offset */,
+  resultMatrixBufferSize /* size */
+);
+
+// Submit GPU commands.
+const gpuCommands = commandEncoder.finish();
+device.queue.submit([gpuCommands]);
+
+console.log('device.queue.submit([gpuCommands]); ist auch durch');
+
+// Read buffer.
+await gpuReadBuffer.mapAsync(GPUMapMode.READ);
+const arrayBuffer = gpuReadBuffer.getMappedRange();
+console.log(new Float32Array(arrayBuffer));
+
 
 
 })();
